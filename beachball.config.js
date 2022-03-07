f26b29fdc17d2588ea76c95b13f641ac53fb6933
Oleksandr Fediashov <olfedias@microsoft.com>
2022-03-07 17:20:09 +0100

    add beachball config

--- beachball.config.js
+++ beachball.config.js
@@ -1,5 +1,45 @@
 // @ts-check
 
+const childProcess = require('child_process');
+
+/**
+ * @param {String} command
+ *
+ * @returns {Promise<string>}
+ */
+function sh(command) {
+  return new Promise((resolve, reject) => {
+    const [cmd, ...args] = command.split(' ');
+
+    /** @type {import('child_process').SpawnOptions} */
+    const options = {
+      cwd: process.cwd(),
+      env: process.env,
+      stdio: 'inherit',
+      shell: true,
+    };
+    const child = childProcess.spawn(cmd, args, options);
+
+    let stdoutData = '';
+
+    if (child.stdout) {
+      child.stdout.on('data', data => {
+        stdoutData += data;
+      });
+    }
+
+    child.on('close', code => {
+      if (code === 0) {
+        resolve(stdoutData);
+      }
+
+      reject(new Error([`child process exited with code ${code}`, stdoutData].join('\n')));
+    });
+  });
+}
+
+let completedPrepublish = false;
+
 /**
  * @type {import('beachball').BeachballConfig}
  */
@@ -9,4 +49,14 @@ module.exports = {
   registry: 'http://localhost:4873/',
   access: 'public',
   branch: 'origin/test-publish',
+  hooks: {
+    async prepublish() {
+      if (!completedPrepublish) {
+        await sh('yarn');
+        await sh('yarn prepublish');
+
+        completedPrepublish = true;
+      }
+    },
+  },
 };
