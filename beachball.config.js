a972bed14c3c4b917279cb0a1d219a5e3d262474
Oleksandr Fediashov <olfedias@microsoft.com>
2022-03-08 14:08:56 +0100

    chore: fix publishing with beachball

--- beachball.config.js
+++ beachball.config.js
@@ -1,8 +1,10 @@
-// @ts-check
-
 /**
  * @type {import('beachball').BeachballConfig}
  */
 module.exports = {
   gitTags: false,
+  registry: 'http://localhost:4873/',
+  access: 'public',
+  branch: 'origin/test-publish',
+  hooks: require('./beachball.hooks'),
 };
